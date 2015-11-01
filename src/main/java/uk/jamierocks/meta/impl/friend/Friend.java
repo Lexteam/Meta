ffe0b05405b8840a7cc534a3a799f39525531f39
Ethan Riley <riley787cmplex@gmail.com>
2015-11-01 00:50:08 +0000

    fixed friend system

--- src/main/java/uk/jamierocks/meta/impl/friend/Friend.java
+++ src/main/java/uk/jamierocks/meta/impl/friend/Friend.java
@@ -23,40 +23,13 @@
  */
 
 package uk.jamierocks.meta.impl.friend;
-import java.util.Set;
 
-public abstract class Friend {
+public class Friend {
 
-    public Friend(){
-        id = new Identity(this);
-    }
+    static public class Identity { private Identity(){}}
 
-    /**
-     * Adds a friend to the class.
-     * @param F friend to add
-     */
-    public void addFriend(Friend F){
-        FriendsOf.add(F.getIdentity().getType());
-    }
+    private static final Identity IDENTITY = new Identity();
 
-    /**
-     * @param F Friend to check.
-     * @returns true if F is a friend to this.
-     */
-    public boolean isFriend(Friend F){
-        return FriendsOf.contains(F.getIdentity().getType());
-    }
+    public Identity getIdentity(){return IDENTITY;}
 
-
-    /**
-     * @return the identity of the class.
-     */
-
-    Identity getIdentity(){
-        return  id;
-    }
-
-    static private Identity id;
-
-    static private Set<String> friendsOf;
 }
